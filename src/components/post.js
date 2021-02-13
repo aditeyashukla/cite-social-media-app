446fe1a03539ed8e69becb19ca7ec1a3d50393b2
aditeyashukla <aditeyashukla@gmail.com>
2021-02-14 00:12:51 +0530

    made newsfeed structure, working on post.js

--- src/components/post.js
+++ src/components/post.js
@@ -1,16 +1,24 @@
 /*
 Component to display and interact with a specific post
  */
-import React, { useContext } from "react";
-import { UserContext } from "../providers/UserProvider";
+import React from "react";
+import Paper from '@material-ui/core/Paper';
 
-export default function NewPost() {
-    const user = useContext(UserContext);
-    const postID = this.props.match.params.PID;
-
+export default function Post(data) {
+    let postData =  data['data'];
+    let pid = data['pid'];
     return (
         <>
-            Make Post page
+            <React.Fragment key={pid}>
+
+                <Paper elevation={3}  >
+
+                    <h1 style={{fontFamily: 'Newsreader, serif'}}>{postData['caption']}</h1>
+                    <p> - {postData['userName']}</p>
+
+                </Paper>
+
+            </React.Fragment>
         </>
 
     );
